dd6f27ca87fd0f25b7956a8f97cb8e796237837b
Edward <edward821220@gmail.com>
2022-11-18 14:31:21 +0800

    layout findTeacher page

--- src/pages/findTeachers/index.tsx
+++ src/pages/findTeachers/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../../../lib/firebase";
 import BearAvatar from "../../../public/member.png";
@@ -29,22 +30,22 @@ const Container = styled.div`
 const TeachersList = styled.ul`
   display: flex;
   flex-wrap: wrap;
-  width: 50%;
+  width: 60%;
 `;
 const Teacher = styled.li`
   flex-basis: 100%;
   display: flex;
-  border: 2px solid ${(props) => props.theme.colors.color2};
+  border: 1px solid lightgray;
   border-radius: 5px;
   margin-bottom: 20px;
-  padding: 10px 5px;
+  padding: 20px;
   background-color: ${(props) => props.theme.colors.color1};
   &:last-child {
     margin-bottom: 0;
   }
 `;
 const TeacherAvatar = styled.div`
-  margin: 0 10px;
+  margin-right: 30px;
   flex-basis: 15%;
 `;
 const TeacherInfo = styled.div`
@@ -53,15 +54,22 @@ const TeacherInfo = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  color: ${(props) => props.theme.colors.color2};
+  color: ${(props) => props.theme.colors.color7};
 `;
 const TeacherName = styled.p`
   font-size: 18px;
   font-weight: bold;
   margin-bottom: 10px;
+  color: ${(props) => props.theme.colors.color2};
 `;
-const TeacherIntroduction = styled.p`
+const TeacherIntroduction = styled.p<{ showMore: string | number }>`
   margin-bottom: 10px;
+  margin-right: 30px;
+  height: ${(props) => (props.showMore ? "auto" : "97.8px")};
+  overflow: hidden;
+  p {
+    line-height: 24px;
+  }
 `;
 
 const TeacherScore = styled.div`
@@ -71,6 +79,28 @@ const TeacherReviewsInfo = styled.p``;
 const StarIcons = styled.div`
   display: flex;
   margin-right: 10px;
+`;
+const Button = styled.button`
+  display: block;
+  background-color: ${(props) => props.theme.colors.color3};
+  color: ${(props) => props.theme.colors.color1};
+  border-radius: 5px;
+  min-width: 80px;
+  height: 40px;
+  padding: 5px 10px;
+  margin-bottom: 10px;
+  cursor: pointer;
+`;
+const ShowMoreButton = styled.div`
+  font-weight: bold;
+  color: ${(props) => props.theme.colors.color6};
+  margin-bottom: 10px;
+  margin-right: 50px;
+  text-align: right;
+  cursor: pointer;
+  &:hover {
+    color: ${(props) => props.theme.colors.color2};
+  }
 `;
 const StarWrapper = styled.div`
   position: relative;
@@ -83,22 +113,35 @@ interface TeacherInterface {
   uid: string;
   reviews: { score: number }[];
   avatar: string;
+  introduction: string;
+  exprience: string;
 }
 
 function FindTeachers() {
   const [teachersList, setTeachersList] = useState<TeacherInterface[]>([]);
+  const [showMore, setShowMore] = useState<number>();
+  const router = useRouter();
   useEffect(() => {
     const getTeachersList = async () => {
       const usersRef = collection(db, "users");
       const teachersQuery = query(usersRef, where("identity", "==", "teacher"));
       const querySnapshot = await getDocs(teachersQuery);
-      const results: { name: string; uid: string; reviews: { score: number }[]; avatar: string }[] = [];
+      const results: {
+        name: string;
+        uid: string;
+        reviews: { score: number }[];
+        avatar: string;
+        introduction: string;
+        exprience: string;
+      }[] = [];
       querySnapshot.forEach((data) => {
         results.push({
           uid: data.data().uid,
           name: data.data().username,
           reviews: data.data().reviews,
           avatar: data.data().photoURL,
+          introduction: data.data().teacher_introduction,
+          exprience: data.data().teacher_exprience,
         });
       });
       setTeachersList(results);
@@ -109,7 +152,7 @@ function FindTeachers() {
     <Wrapper>
       <Container>
         <TeachersList>
-          {teachersList.map((teacher) => (
+          {teachersList.map((teacher, index) => (
             <Teacher key={teacher.uid}>
               <TeacherAvatar>
                 <Link href={`/findTeachers/reserve/${teacher.uid}`}>
@@ -118,9 +161,19 @@ function FindTeachers() {
               </TeacherAvatar>
               <TeacherInfo>
                 <TeacherName>{teacher.name}</TeacherName>
-                <TeacherIntroduction>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae placeat quibusdam veniam laudantium.
-                </TeacherIntroduction>
+                <TeacherIntroduction
+                  showMore={showMore === index ? showMore : ""}
+                  dangerouslySetInnerHTML={{
+                    __html: `${teacher.introduction}<p style='margin:10px 0; color:#654116'>老師經歷</p>${teacher.exprience}`,
+                  }}
+                />
+                <ShowMoreButton
+                  onClick={() => {
+                    setShowMore(index);
+                  }}
+                >
+                  Show more
+                </ShowMoreButton>
                 {teacher?.reviews?.length > 0 ? (
                   <TeacherScore>
                     <StarIcons>
@@ -155,6 +208,13 @@ function FindTeachers() {
                   <TeacherReviewsInfo>目前無評價</TeacherReviewsInfo>
                 )}
               </TeacherInfo>
+              <Button
+                onClick={() => {
+                  router.push(`/findTeachers/reserve/${teacher.uid}`);
+                }}
+              >
+                立刻預約
+              </Button>
             </Teacher>
           ))}
         </TeachersList>
